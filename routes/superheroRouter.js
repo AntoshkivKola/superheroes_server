eb867cca24afd2b42e0ce6458da342555765b3fe
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-04-11 10:01:16 +0300

    Start work

---
+++ routes/superheroRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+// const UserController = require('../controller/user.controller');
+const paginate = require('../middlewares/paginate.mw');
+
+const userRouter = Router();
+
+module.exports = userRouter;
