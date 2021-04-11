2f57b4ef5a6645718ca5b1645754dbef7292fd59
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-04-11 23:45:45 +0300

    Add delete image

--- controller/image.controller.js
+++ controller/image.controller.js
@@ -1,5 +1,8 @@
 const createError = require('http-errors');
 const { Superhero, Image } = require('../models');
+const fs = require('fs');
+const { pathToImages } = require('../middlewares/file.upload');
+const path = require('path');
 
 module.exports.createImage = async (req, res, next) => {
   try {
@@ -10,7 +13,7 @@ module.exports.createImage = async (req, res, next) => {
 
     const creatImageValues = files.map(file => {
       return (file = {
-        imagePath: file.name,
+        imagePath: file.filename,
         superheroId,
       });
     });
@@ -33,15 +36,42 @@ module.exports.getAllSuperheroImages = async (req, res, next) => {
       params: { superheroId },
     } = req;
 
-    const superhero = await Superhero.findByPk(superheroId)
-    
+    const superhero = await Superhero.findByPk(superheroId);
+
     const images = await superhero.getImages();
-    
+
     if (!images) {
       return next(createError(404, 'Images not found'));
     }
 
     res.send(images);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteImage = async (req, res, next) => {
+  try {
+    const {
+      params: { imageId },
+    } = req;
+
+    const image = await Image.findByPk(imageId);
+
+    const deletePath = path.resolve(pathToImages, image.imagePath);
+    console.log(pathToImages, image.imagePath, deletePath);
+
+    fs.unlinkSync(deletePath);
+
+    const rowsCount = await Image.destroy({
+      where: { id: imageId },
+    });
+
+    if (rowsCount !== 1) {
+      return next(createError(400, 'Image cant be deleted'));
+    }
+
+    res.send({ data: imageId });
   } catch (err) {
     next(err);
   }
